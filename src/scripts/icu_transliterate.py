a4bb4a886b2d6410578a665cef01e4a70265c84e
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-05-19 11:48:06 -0400

    Create icu_transliterate.py

---
+++ src/scripts/icu_transliterate.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import icu
+
+tl = icu.Transliterator.createInstance('Any-Latin; Latin-ASCII')
+
+with open(os.path.abspath(sys.argv[1]), "r") as r, open(os.path.abspath(sys.argv[2]), "w") as w:
+    for i, line in enumerate(r):
+        transliteration = tl.transliterate(line.strip())
+        w.write(transliteration)
+        w.write("\n")
+        print(i, end="\r")
+print("\n Finished!")
+
+
