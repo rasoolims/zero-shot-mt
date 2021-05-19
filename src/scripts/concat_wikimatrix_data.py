8e1ab85e2efe49cf4cdedb4827ccf8432c6e4b7d
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-05-19 12:26:02 -0400

    Update concat_wikimatrix_data.py

--- src/scripts/concat_wikimatrix_data.py
+++ src/scripts/concat_wikimatrix_data.py
@@ -7,7 +7,14 @@ with open(output_file + ".en", "w") as enw, open(output_file + ".src", "w") as s
     for file in os.listdir(input_dir):
         if file.endswith("tsv"):
             en_src = "en-" in file  # English is source!
-            print(file)
+            if en_src:
+                prefix_len = len("WikiMatrix.en-")
+                dst_lang_id = "<" + file[prefix_len:file.rfind(".")] + ">"
+            else:
+                prefix_len = len("WikiMatrix.")
+                dst_lang_id = "<" + file[prefix_len:file.rfind("-")] + ">"
+
+            print(file, dst_lang_id)
             with open(os.path.join(input_dir, file), "r") as r:
                 for line in r:
                     spl = line.strip().split("\t")
@@ -18,6 +25,8 @@ with open(output_file + ".en", "w") as enw, open(output_file + ".src", "w") as s
                         en_sen, other_sen = spl[2].strip(), spl[1].strip()
 
                     if len(en_sen) > 1 and len(other_sen) > 1:
+                        en_sen = " ".join(["<en>", en_sen, "</s>"])
+                        other_sen = " ".join([dst_lang_id, other_sen, "</s>"])
                         enw.write(en_sen)
                         enw.write("\n")
                         srcw.write(other_sen)
