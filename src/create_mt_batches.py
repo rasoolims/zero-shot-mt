bc70db6b95c65ee8ffef9d0d303adfeef042ecd2
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-05-20 14:32:33 -0400

    Batching more efficiently

--- src/create_mt_batches.py
+++ src/create_mt_batches.py
@@ -7,7 +7,7 @@ from transformers import XLMRobertaTokenizer
 from textprocessor import TextProcessor
 
 
-def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, src_lang: int, srct_txt_file: str = None,
+def write(tp: TextProcessor, output_file: str, src_txt_file: str, src_lang: int, srct_txt_file: str = None,
           dst_txt_file: str = None, dst_lang: int = None, min_len: int = 1, max_len: int = 175, shallow: bool = False):
     """
     There are scenarios for which the input comes from two streams such as original text and transliterated text. Or we
@@ -18,76 +18,42 @@ def write(text_processor: TextProcessor, output_file: str, src_txt_file: str, sr
         tokenizer_class, weights = XLMRobertaTokenizer, 'xlm-roberta-base'
         tokenizer = tokenizer_class.from_pretrained(weights)
     else:
-        tokenizer = text_processor
+        tokenizer = tp
 
-    examples = {}
-    line_num = 0
-    lens = {}
-    if srct_txt_file is not None:
-        dst_lang_str = text_processor.languages[text_processor.id2token(dst_lang)]
-        with open(src_txt_file, "r") as s_fp, open(dst_txt_file, "r") as d_fp, open(srct_txt_file, "r") as st_fp:
-            for src_line, dst_line, srct_line in zip(s_fp, d_fp, st_fp):
-                if len(src_line.strip()) == 0 or len(dst_line.strip()) == 0: continue
-                if not shallow:
-                    src_tok_line = tokenizer.encode(src_line.strip())
-                else:
-                    src_tok_line = text_processor.tokenize_one_sentence_with_langid(src_line.strip(), src_lang)
+    print(datetime.datetime.now(), "Reading source lines!")
+    with open(src_txt_file, "r") as s_fp:
+        src_lines = list(map(lambda x: x.strip(), s_fp))
+    print(datetime.datetime.now(), "Reading target lines!")
+    with open(dst_txt_file, "r") as d_fp:
+        dst_lines = list(map(lambda x: x.strip(), d_fp))
 
-                srct_tok_line = text_processor.tokenize_one_sentence(srct_line.strip())
-                dst_tok_line = text_processor.tokenize_one_sentence_with_langid(dst_line.strip(), dst_lang)
-
-                if min_len <= len(src_tok_line) <= max_len and min_len <= len(
-                        dst_tok_line) <= max_len and min_len <= len(srct_tok_line) <= max_len:
-                    examples[line_num] = (src_tok_line, dst_tok_line, dst_lang_str, srct_tok_line)
-                    lens[line_num] = len(dst_tok_line)
-                    line_num += 1
-
-                if line_num % 1000 == 0:
-                    print(line_num, end="\r")
-
-        print("\nSorting")
-        sorted_lens = sorted(lens.items(), key=lambda item: item[1])
-        sorted_examples = []
-        print("Sorted examples")
-        for len_item in sorted_lens:
-            line_num = len(sorted_examples)
-            sorted_examples.append(examples[len_item[0]])
-
-        print("Dumping")
-        with open(output_file, "wb") as fw:
-            marshal.dump(sorted_examples, fw)
+    print(datetime.datetime.now(), "Reading source-translitered lines!")
+    if srct_txt_file is None:
+        srct_lines = src_lines
     else:
-        dst_lang_str = text_processor.languages[text_processor.id2token(dst_lang)]
-        with open(src_txt_file, "r") as s_fp, open(dst_txt_file, "r") as d_fp:
-            for src_line, dst_line in zip(s_fp, d_fp):
-                if len(src_line.strip()) == 0 or len(dst_line.strip()) == 0: continue
-                if not shallow:
-                    src_tok_line = tokenizer.encode(src_line.strip())
-                else:
-                    src_tok_line = text_processor.tokenize_one_sentence_with_langid(src_line.strip(), src_lang)
+        with open(srct_txt_file, "r") as st_fp:
+            srct_lines = list(map(lambda x: x.strip(), st_fp))
 
-                srct_tok_line = text_processor.tokenize_one_sentence(src_line.strip())
-                dst_tok_line = text_processor.tokenize_one_sentence_with_langid(dst_line.strip(), dst_lang)
+    assert len(src_lines) == len(dst_lines) == len(srct_lines)
+    print(datetime.datetime.now(), "Number of parallel sentences:", len(dst_lines))
+    src_encode = lambda x: tp.tokenize_one_sentence_with_langid(x, src_lang) if shallow else tokenizer.encode(x)
+    target_encode = lambda x: tp.tokenize_one_sentence_with_langid(x, dst_lang)
+    srct_enclode = lambda x: tp.tokenize_one_sentence(x)
 
-                if min_len <= len(src_tok_line) <= max_len and min_len <= len(dst_tok_line) <= max_len:
-                    examples[line_num] = (src_tok_line, dst_tok_line, dst_lang_str, srct_tok_line)
-                    lens[line_num] = len(dst_tok_line)
-                    line_num += 1
-
-                if line_num % 1000 == 0:
-                    print(line_num, end="\r")
-
-        print("\nSorting")
-        sorted_lens = sorted(lens.items(), key=lambda item: item[1])
-        sorted_examples = []
-        print("Sorted examples")
-        for len_item in sorted_lens:
-            line_num = len(sorted_examples)
-            sorted_examples.append(examples[len_item[0]])
-
-        print("Dumping")
-        with open(output_file, "wb") as fw:
-            marshal.dump(sorted_examples, fw)
+    print(datetime.datetime.now(), "Tokenizing examples!")
+    examples = list(map(lambda x: (src_encode(x[0]), target_encode(x[1]), srct_enclode(x[2])),
+                        zip(src_lines, dst_lines, dst_lines)))
+    print(datetime.datetime.now(), "Getting example lengths!")
+    example_length = dict(map(lambda e: (e[0], len(e[1])), enumerate(examples)))
+    print(datetime.datetime.now(), "Sorting example lengths!")
+    sorted_lens = sorted(example_length.items(), key=lambda item: item[1])
+    print(datetime.datetime.now(), "Getting sorted examples!")
+    sorted_examples = list(map(lambda i: examples[i[0]], sorted_lens))
+    print(datetime.datetime.now(), "Dumping sorted examples!")
+    with open(output_file, "wb") as fw:
+        dst_lang_id = tp.languages[tp.id2token(dst_lang)]
+        marshal.dump((sorted_examples, dst_lang_id), fw)
+    print(datetime.datetime.now(), "Finished!")
 
 
 def get_options():
@@ -116,10 +82,8 @@ if __name__ == "__main__":
     options = get_options()
     tokenizer = TextProcessor(options.tokenizer_path)
 
-    print(datetime.datetime.now(), "Writing batches")
     src_lang = None if options.src_lang is None else tokenizer.token_id("<" + options.src_lang + ">")
     dst_lang = tokenizer.token_id("<" + options.dst_lang + ">") if options.dst_lang is not None else None
-    write(text_processor=tokenizer, output_file=options.output_path, src_txt_file=options.src_data_path,
+    write(tp=tokenizer, output_file=options.output_path, src_txt_file=options.src_data_path,
           srct_txt_file=options.srct_data_path, src_lang=src_lang,
           dst_txt_file=options.dst_data_path, dst_lang=dst_lang, shallow=options.shallow_encoder)
-    print(datetime.datetime.now(), "Finished")
--- src/dataset.py
+++ src/dataset.py
@@ -14,15 +14,11 @@ logger = logging.getLogger(__name__)
 class MTDataset(Dataset):
     def __init__(self, max_batch_capacity: int, max_batch: int,
                  src_pad_idx: int, dst_pad_idx: int, max_seq_len: int = 175, batch_pickle_dir: str = None,
-                 examples: List[Tuple[torch.tensor, torch.tensor, int, int]] = None, keep_src_pad_idx=True,
-                 ngpu=1):
+                 keep_src_pad_idx=True, ngpu=1):
         self.keep_src_pad_idx = keep_src_pad_idx
         self.ngpu = ngpu
 
-        if examples is None:
-            self.build_batches(batch_pickle_dir, max_batch_capacity, max_batch, src_pad_idx, dst_pad_idx, max_seq_len)
-        else:
-            self.batch_examples(examples, max_batch, max_batch_capacity, max_seq_len, ngpu, src_pad_idx, dst_pad_idx)
+        self.build_batches(batch_pickle_dir, max_batch_capacity, max_batch, src_pad_idx, dst_pad_idx, max_seq_len)
 
     def build_batches(self, batch_pickle_dir: str, max_batch_capacity: int, max_batch: int,
                       src_pad_idx: int, dst_pad_idx: int, max_seq_len: int = 175):
@@ -34,79 +30,76 @@ class MTDataset(Dataset):
         """
         with open(batch_pickle_dir, "rb") as fr:
             print("LOADING MT BATCHES")
-            examples: List[Tuple[torch.tensor, torch.tensor, int, int]] = marshal.load(fr)
-            self.batch_examples(examples, max_batch, max_batch_capacity, max_seq_len, self.ngpu, src_pad_idx,
-                                dst_pad_idx)
+            (examples, dst_language) = marshal.load(fr)
 
-    def batch_examples(self, examples, max_batch, max_batch_capacity, max_seq_len, num_gpu, src_pad_idx, dst_pad_idx):
-        print("BUILDING MT BATCHES")
-        self.batches = []
-        cur_src_batch, cur_srct_batch, cur_dst_batch = [], [], []
-        cur_max_src_len, cur_max_srct_len, cur_max_dst_len = 0, 0, 0
-        cur_dst_langs, cur_lex_cand_batch = [], []
-        for ei, example in enumerate(examples):
-            src = torch.LongTensor(example[0][:max_seq_len])  # trim if longer than expected!
-            dst = torch.LongTensor(example[1][:max_seq_len])  # trim if longer than expected!
-            srct = torch.LongTensor(example[3][:max_seq_len])  # trim if longer than expected!
-            cur_dst_langs.append(example[2])
-            cur_max_src_len = max(cur_max_src_len, int(src.size(0)))
-            cur_max_srct_len = max(cur_max_srct_len, int(srct.size(0)))
-            cur_max_dst_len = max(cur_max_dst_len, int(dst.size(0)))
+            print("BUILDING MT BATCHES")
+            self.batches = []
+            cur_src_batch, cur_srct_batch, cur_dst_batch = [], [], []
+            cur_max_src_len, cur_max_srct_len, cur_max_dst_len = 0, 0, 0
+            cur_dst_langs = []
+            for ei, example in enumerate(examples):
+                src = torch.LongTensor(example[0][:max_seq_len])  # trim if longer than expected!
+                dst = torch.LongTensor(example[1][:max_seq_len])  # trim if longer than expected!
+                srct = torch.LongTensor(example[2][:max_seq_len])  # trim if longer than expected!
+                cur_dst_langs.append(dst_language)
+                cur_max_src_len = max(cur_max_src_len, int(src.size(0)))
+                cur_max_srct_len = max(cur_max_srct_len, int(srct.size(0)))
+                cur_max_dst_len = max(cur_max_dst_len, int(dst.size(0)))
 
-            cur_src_batch.append(src)
-            cur_srct_batch.append(srct)
-            cur_dst_batch.append(dst)
+                cur_src_batch.append(src)
+                cur_srct_batch.append(srct)
+                cur_dst_batch.append(dst)
 
-            batch_capacity_size = (cur_max_src_len ** 2 + cur_max_dst_len ** 2) * len(
-                cur_src_batch) * cur_max_dst_len
-            batch_size = (cur_max_src_len + cur_max_dst_len) * len(cur_src_batch)
+                batch_capacity_size = (cur_max_src_len ** 2 + cur_max_dst_len ** 2) * len(
+                    cur_src_batch) * cur_max_dst_len
+                batch_size = (cur_max_src_len + cur_max_dst_len) * len(cur_src_batch)
 
-            if (batch_size > max_batch or batch_capacity_size > max_batch_capacity * 1000000) and \
-                    len(cur_src_batch[:-1]) >= num_gpu and len(cur_src_batch) > 1:
-                src_batch = pad_sequence(cur_src_batch[:-1], batch_first=True, padding_value=src_pad_idx)
+                if (batch_size > max_batch or batch_capacity_size > max_batch_capacity * 1000000) and \
+                        len(cur_src_batch[:-1]) >= self.ngpu and len(cur_src_batch) > 1:
+                    src_batch = pad_sequence(cur_src_batch[:-1], batch_first=True, padding_value=src_pad_idx)
+                    # Bellow should be dst_pad_idx since srct has the same tokenizer as the target output.
+                    srct_batch = pad_sequence(cur_srct_batch[:-1], batch_first=True, padding_value=dst_pad_idx)
+                    dst_batch = pad_sequence(cur_dst_batch[:-1], batch_first=True, padding_value=dst_pad_idx)
+                    src_pad_mask = (src_batch != src_pad_idx)
+                    # Bellow should be dst_pad_idx since srct has the same tokenizer as the target output.
+                    srct_pad_mask = (srct_batch != dst_pad_idx)
+                    dst_pad_mask = (dst_batch != dst_pad_idx)
+
+                    entry = {"src_texts": src_batch, "srct_texts": srct_batch, "src_pad_mask": src_pad_mask,
+                             "dst_texts": dst_batch, "srct_pad_mask": srct_pad_mask, "dst_pad_mask": dst_pad_mask,
+                             "dst_langs": torch.LongTensor(cur_dst_langs[:-1])}
+                    self.batches.append(entry)
+                    cur_src_batch, cur_srct_batch = [cur_src_batch[-1]], [cur_srct_batch[-1]]
+                    cur_dst_batch, cur_dst_langs = [cur_dst_batch[-1]], [cur_dst_langs[-1]]
+                    cur_max_src_len, cur_max_srct_len, cur_max_dst_len = int(cur_src_batch[0].size(0)), int(
+                        cur_srct_batch[0].size(0)), int(cur_dst_batch[0].size(0))
+
+                if ei % 1000 == 0:
+                    print(ei, "/", len(examples), end="\r")
+
+            if len(cur_src_batch) > 0 and len(cur_src_batch) >= self.ngpu:
+                src_batch = pad_sequence(cur_src_batch, batch_first=True, padding_value=src_pad_idx)
                 # Bellow should be dst_pad_idx since srct has the same tokenizer as the target output.
-                srct_batch = pad_sequence(cur_srct_batch[:-1], batch_first=True, padding_value=dst_pad_idx)
-                dst_batch = pad_sequence(cur_dst_batch[:-1], batch_first=True, padding_value=dst_pad_idx)
+                srct_batch = pad_sequence(cur_srct_batch, batch_first=True, padding_value=dst_pad_idx)
+                dst_batch = pad_sequence(cur_dst_batch, batch_first=True, padding_value=dst_pad_idx)
                 src_pad_mask = (src_batch != src_pad_idx)
                 # Bellow should be dst_pad_idx since srct has the same tokenizer as the target output.
                 srct_pad_mask = (srct_batch != dst_pad_idx)
                 dst_pad_mask = (dst_batch != dst_pad_idx)
-
-                entry = {"src_texts": src_batch, "srct_texts": srct_batch, "src_pad_mask": src_pad_mask,
-                         "dst_texts": dst_batch, "srct_pad_mask": srct_pad_mask, "dst_pad_mask": dst_pad_mask,
-                         "dst_langs": torch.LongTensor(cur_dst_langs[:-1])}
+                entry = {"src_texts": src_batch, "src_pad_mask": src_pad_mask, "dst_texts": dst_batch,
+                         "srct_texts": srct_batch, "srct_pad_mask": srct_pad_mask,
+                         "dst_pad_mask": dst_pad_mask, "dst_langs": torch.LongTensor(cur_dst_langs)}
                 self.batches.append(entry)
-                cur_src_batch, cur_srct_batch = [cur_src_batch[-1]], [cur_srct_batch[-1]]
-                cur_dst_batch, cur_dst_langs = [cur_dst_batch[-1]], [cur_dst_langs[-1]]
-                cur_max_src_len, cur_max_srct_len, cur_max_dst_len = int(cur_src_batch[0].size(0)), int(
-                    cur_srct_batch[0].size(0)), int(cur_dst_batch[0].size(0))
 
-            if ei % 1000 == 0:
-                print(ei, "/", len(examples), end="\r")
+            if self.keep_src_pad_idx:
+                src_pad_idx_find = lambda non_zeros, sz: (sz - 1) if int(non_zeros.size(0)) == 0 else non_zeros[0]
+                for b in self.batches:
+                    pads = b["src_texts"] == src_pad_idx
+                    sz = int(pads.size(1))
+                    pad_indices = torch.LongTensor(list(map(lambda p: src_pad_idx_find(torch.nonzero(p), sz), pads)))
+                    b["src_pad_idx"] = pad_indices
 
-        if len(cur_src_batch) > 0 and len(cur_src_batch) >= num_gpu:
-            src_batch = pad_sequence(cur_src_batch, batch_first=True, padding_value=src_pad_idx)
-            # Bellow should be dst_pad_idx since srct has the same tokenizer as the target output.
-            srct_batch = pad_sequence(cur_srct_batch, batch_first=True, padding_value=dst_pad_idx)
-            dst_batch = pad_sequence(cur_dst_batch, batch_first=True, padding_value=dst_pad_idx)
-            src_pad_mask = (src_batch != src_pad_idx)
-            # Bellow should be dst_pad_idx since srct has the same tokenizer as the target output.
-            srct_pad_mask = (srct_batch != dst_pad_idx)
-            dst_pad_mask = (dst_batch != dst_pad_idx)
-            entry = {"src_texts": src_batch, "src_pad_mask": src_pad_mask, "dst_texts": dst_batch,
-                     "srct_texts": srct_batch, "srct_pad_mask": srct_pad_mask,
-                     "dst_pad_mask": dst_pad_mask, "dst_langs": torch.LongTensor(cur_dst_langs)}
-            self.batches.append(entry)
-
-        if self.keep_src_pad_idx:
-            src_pad_idx_find = lambda non_zeros, sz: (sz - 1) if int(non_zeros.size(0)) == 0 else non_zeros[0]
-            for b in self.batches:
-                pads = b["src_texts"] == src_pad_idx
-                sz = int(pads.size(1))
-                pad_indices = torch.LongTensor(list(map(lambda p: src_pad_idx_find(torch.nonzero(p), sz), pads)))
-                b["src_pad_idx"] = pad_indices
-
-        print("\nLoaded %d bitext sentences to %d batches!" % (len(examples), len(self.batches)))
+            print("\nLoaded %d bitext sentences to %d batches!" % (len(examples), len(self.batches)))
 
     def __len__(self):
         return len(self.batches)
