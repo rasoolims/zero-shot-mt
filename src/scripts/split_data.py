b0372a0ee3f54973f16fff1b5efe67c1ebf8e3ba
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-05-20 16:17:31 -0400

    Update split_data.py

--- src/scripts/split_data.py
+++ src/scripts/split_data.py
@@ -11,6 +11,6 @@ num_splits = int(sys.argv[2])
 for s in range(num_splits):
     print(datetime.datetime.now(), "Write split", str(s + 1))
     with open(os.path.abspath(sys.argv[3]) + str(s + 1), "w") as w:
-        for i in range(s, len(lines), step=num_splits):
+        for i in range(s, len(lines), num_splits):
             w.write(lines[i].strip() + "\n")
 print(datetime.datetime.now(), "Finished!")
