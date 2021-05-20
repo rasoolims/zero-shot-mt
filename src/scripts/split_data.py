74053c27ded10bfed232ce7dbed686c1f91e2359
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-05-20 16:09:33 -0400

    Create split_data.py

---
+++ src/scripts/split_data.py
@@ -0,0 +1,16 @@
+import datetime
+import os
+import sys
+
+print(datetime.datetime.now(), "Reading lines!")
+with open(os.path.abspath(sys.argv[1]), "r") as s_fp:
+    lines = list(map(lambda x: x.strip(), s_fp))
+
+num_splits = int(sys.argv[2])
+
+for s in range(num_splits):
+    print(datetime.datetime.now(), "Write split", str(s + 1))
+    with open(os.path.abspath(sys.argv[2]) + str(s + 1), "r") as w:
+        for i in range(s, len(lines), step=num_splits):
+            w.write(lines[i].strip() + "\n")
+print(datetime.datetime.now(), "Finished!")
