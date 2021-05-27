9bc0a9dffd2b107e41666dcad8bfdd76510219cb
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2021-05-27 11:07:25 -0400

    Update train_mt.py

--- src/train_mt.py
+++ src/train_mt.py
@@ -279,8 +279,7 @@ class Trainer:
 
         step, train_epoch = 0, 1
         while options.step > 0 and step < options.step:
-            if trainer.rank <= 0:
-                print("train epoch", train_epoch)
+            print(trainer.rank, "--> train epoch", train_epoch)
             step = trainer.train_epoch(mt_train_iter=mt_train_loader, max_step=options.step,
                                        mt_dev_iter=mt_dev_loader, saving_path=options.model_path, step=step,
                                        save_opt=options.save_opt, accum=options.accum)
